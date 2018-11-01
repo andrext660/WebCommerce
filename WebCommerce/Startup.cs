2a30625971db1a1aaec5b9e323983f820f73c4d9
André Lucas <andre.souza@souunit.com.br>
2018-10-31 21:19:46 -0300

    Adicionar arquivos de projeto.

---
+++ WebCommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCommerce.Startup))]
+namespace WebCommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
