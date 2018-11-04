aa3f32fd308c6235c2becdb4624c633ac9238f14
Matheus Gama <matheus_mgm@hotmail.com>
2018-11-04 15:08:41 -0300

    Editando o fluent API

--- WebCommerce/Models/FluentMaps/CategoriaMap.cs
+++ WebCommerce/Models/FluentMaps/CategoriaMap.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-public class Class1
+namespace WebCommerce.Models.FluentMaps
 {
-	public Class1()
-	{
-	}
+    public class CategoriaMap : EntityTypeConfiguration<Categoria>
+    {
+        public CategoriaMap()
+        {
+            ToTable("Categorias");
+            HasKey(a => a.Id).
+            Property(a => a.Nome).IsRequired().HasColumnType("Varchar").HasColumnName("Nome").HasParameterName("Nome");
+            Property(a => a.Descricao).HasColumnName("Descricao").HasColumnType("Varchar").HasParameterName("Descrição");
+        }
+    }
 }
--- WebCommerce/Models/FluentMaps/ClienteMap.cs
+++ WebCommerce/Models/FluentMaps/ClienteMap.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-public class Class1
+namespace WebCommerce.Models.FluentMaps
 {
-	public Class1()
-	{
-	}
+    public class ClienteMap : EntityTypeConfiguration<Cliente>
+    {
+        public CategoriaMap()
+        {
+            ToTable("Clientes");
+            HasKey(a => a.Id).
+            Property(a => a.Nome).IsRequired().HasColumnType("Varchar").HasColumnName("Nome").HasParameterName("Nome");
+            Property(a => a.DataNascimento).IsRequired().HasColumnType("DateTime").HasColumnName("DataNasc").HasParameterName("Data de Nascimento");
+            Property(a => a.Descricao).HasColumnName("Descricao").HasColumnType("Varchar").HasParameterName("Descrição");
+            Property(a => a.CPF).IsRequired().HasColumnType("Varchar").HasColumnName("CPF").HasParameterName("CPF");
+            Property(a => a.Telefone).IsRequired().HasColumnType("Varchar").HasColumnName("Telefone").HasParameterName("Telefone");
+        }
+    }
 }
