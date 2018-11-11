7f4b2aadea23c5c7a6b110ac5c17a25abf0de355
Raffael de Castro Rodrigues <raffaelcr.ti@gmail.com>
2018-11-11 20:37:06 -0300

    Scafold endereco atualizado
    scafold do endereco depois de mudar os arquivos

---
+++ WebCommerce/Controllers/EnderecosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebCommerce.Models;
+using WebCommerce.Models.Classes;
+
+namespace WebCommerce.Controllers
+{
+    public class EnderecosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Enderecos
+        public ActionResult Index()
+        {
+            return View(db.Enderecoes.ToList());
+        }
+
+        // GET: Enderecos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Endereco endereco = db.Enderecoes.Find(id);
+            if (endereco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(endereco);
+        }
+
+        // GET: Enderecos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enderecos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CEP,Rua,Bairro,Cidade,Estado,Numero")] Endereco endereco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enderecoes.Add(endereco);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(endereco);
+        }
+
+        // GET: Enderecos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Endereco endereco = db.Enderecoes.Find(id);
+            if (endereco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(endereco);
+        }
+
+        // POST: Enderecos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CEP,Rua,Bairro,Cidade,Estado,Numero")] Endereco endereco)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(endereco).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(endereco);
+        }
+
+        // GET: Enderecos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Endereco endereco = db.Enderecoes.Find(id);
+            if (endereco == null)
+            {
+                return HttpNotFound();
+            }
+            return View(endereco);
+        }
+
+        // POST: Enderecos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Endereco endereco = db.Enderecoes.Find(id);
+            db.Enderecoes.Remove(endereco);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
