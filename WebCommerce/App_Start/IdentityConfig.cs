5a022866881290ac952619f3481c99338f7b473f
Fernandoqsiqueira <44657357+Fernandoqsiqueira@users.noreply.github.com>
2018-11-13 13:02:14 -0300

    Mudanças nas regras de login
    retirei as regras quanto a caracteres especiais e letras pra facilitar o acesso, caso ocorra algum bug, retorne ao original

--- WebCommerce/App_Start/IdentityConfig.cs
+++ WebCommerce/App_Start/IdentityConfig.cs
@@ -54,10 +54,10 @@ namespace WebCommerce
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configurar padrões de bloqueio de usuário
