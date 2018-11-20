bb449993122bb79b4a2615bae4172e03566c8ad5
Raffael de Castro Rodrigues <raffaelcr.ti@gmail.com>
2018-11-20 10:00:19 -0300

    Ajustes e db seed
    Ajustado a remoção da propriedade descontoQuantidade de promocao; Descomentação do idEndereco de cliente; Seed de todas as classes no db

--- WebCommerce/Models/Classes/Cliente.cs
+++ WebCommerce/Models/Classes/Cliente.cs
@@ -14,7 +14,7 @@ namespace WebCommerce.Models.Classes
         public DateTime DataNascimento { get; set; }
 		public string CPF { get; set; }
 		public string Telefone { get; set; }
-		//public int? IdEndereco { get; set; }
+		public int? IdEndereco { get; set; }
 		public Endereco Endereco { get; set; }
 
         public virtual ICollection<Venda> HistoricoVenda { get; set; }
--- WebCommerce/Models/DatabaseSeed/WebCommerceDataBaseSeed.cs
+++ WebCommerce/Models/DatabaseSeed/WebCommerceDataBaseSeed.cs
@@ -19,39 +19,70 @@ namespace WebCommerce.Models.DatabaseSeed
                 {
                     try
                     {
-                        //Criado um objeto de cada entidade
-                        Categoria categoria = new Categoria();
-                        Cliente cliente = new Cliente();
-                        Cupom cupom = new Cupom();
-                        Endereco endereco = new Endereco();
-                        Produto produto = new Produto();
-                        Promocao promocao = new Promocao();
-                        Venda venda = new Venda();
+						//Criado um objeto de cada entidade
+						Categoria categoria = new Categoria();
+						Cliente cliente = new Cliente();
+						Cupom cupom = new Cupom();
+						Endereco endereco = new Endereco();
+						Produto produto = new Produto();
+						Promocao promocao = new Promocao();
+						Venda venda = new Venda();
+						
+						cupom.DescontoPorcentagem = 10;
+						cupom.Descricao = "Desconto Teste";
+						cupom.Quantidade = 7;
+						cupom.Valido = true;
+						contexto.Cupoms.Add(cupom);
 
-                        //Implementado as infomrações do endereco e salvo no contexto
-                        endereco.Bairro = "Aruana";
-                        endereco.CEP = "49038443";
-                        endereco.Cidade = "Aracaju";
-                        endereco.Numero = 4000;
-                        endereco.Rua = "Avenida Melicio Machado";
-                        endereco.UF = "SE";
-                        contexto.Enderecoes.Add(endereco);
-                        //Realizado o savechanges para salvar o endereco e poder inclui-lo no cliente
-                        contexto.SaveChanges();
+						categoria.Nome = "Computador Gamer";
+						categoria.Descricao = "Computadores com especificações técnicas mais avançadas";
+						contexto.Categorias.Add(categoria);
 
-                        cliente.CPF = "07177972528";
-                        cliente.DataNascimento = DateTime.Parse("18/04/1997");
-                        cliente.Endereco = endereco;
-                        //cliente.Endereco = new Endereco();
-                        //cliente.Endereco.Id = endereco.Id;
-                        cliente.Nome = "Raffael";
-                        cliente.Telefone = "991554964";
-                        contexto.Clientes.Add(cliente);
+						promocao.DataPrazo = DateTime.Parse("25/12/2018");
+						promocao.DescontoPorcentagem = 5;
+						promocao.Descricao = "Desconto Teste";
+						promocao.Valido = true;
+						contexto.Promocaos.Add(promocao);
 
-                        contexto.SaveChanges();
+						context.SaveChanges();
 
+						venda.IdCliente = cliente.Id;
+						contexto.Vendas.Add(venda);
 
-                    }
+						context.SaveChanges();
+
+						//Implementado as infomrações do endereco e salvo no contexto
+						endereco.Bairro = "Aruana";
+						endereco.CEP = "49038443";
+						endereco.Cidade = "Aracaju";
+						endereco.Numero = 4000;
+						endereco.Rua = "Avenida Melicio Machado";
+						endereco.UF = "SE";
+						contexto.Enderecoes.Add(endereco);
+						//Realizado o savechanges para salvar o endereco e poder inclui-lo no cliente
+						contexto.SaveChanges();
+
+						cliente.CPF = "07177972528";
+						cliente.DataNascimento = DateTime.Parse("18/04/1997");
+						cliente.Endereco = endereco;
+						cliente.IdEndereco = endereco.Id;
+						cliente.Nome = "Raffael";
+						cliente.Telefone = "991554964";
+						contexto.Clientes.Add(cliente);
+
+						produto.IdCategoria = categoria.Id;
+						produto.IdPromocao = promocao.Id;
+						produto.Nome = "PC GAMER GTX";
+						produto.Preco = 4500;
+						produto.Promocao = promocao;
+						produto.Categoria = categoria;
+						produto.Detalhes = "Esse pc é pra gamer apenas, sai xiu";
+						produto.QuantidadeDisponivel = 10;
+						contexto.Produtoes.Add(produto);
+
+						contexto.SaveChanges();
+
+					}
                     catch (Exception)
                     {
                         dbTransaction.Rollback();
