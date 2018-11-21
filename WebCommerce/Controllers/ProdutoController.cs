abd367d51aef78b98db4d38b2ab237b94a503e8d
Fernandoqsiqueira <44657357+Fernandoqsiqueira@users.noreply.github.com>
2018-11-21 18:20:55 -0300

    Revert "Redirecionamento Customizado para usuário sem permissão"
    This reverts commit 5502a473cda0eaf868fc310b8c8dfa9e0d20ec4c.

--- WebCommerce/Controllers/ProdutoController.cs
+++ WebCommerce/Controllers/ProdutoController.cs
@@ -96,7 +96,7 @@ namespace WebCommerce.Controllers
 
         // GET: Produto/Edit/5
 
-        [CustomAuthorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")]
 
         public ActionResult Edit(int? id)
         {
@@ -129,7 +129,7 @@ namespace WebCommerce.Controllers
         }
 
         // GET: Produto/Delete/5
-        [CustomAuthorize(Roles = "Admin")]
+        [Authorize(Roles = "Admin")]
         public ActionResult Delete(int? id)
         {
             if (id == null)
