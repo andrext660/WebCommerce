b41ac23017fe825f6347288e2847a29996b1545a
Deivide Mateus <deivide.mateus@hotmail.com>
2018-11-22 00:10:51 -0300

    Confirmção de email
    Nao está funcionando com emails microsoft (testei com hotmail e outlook), pelo menos não aqui, nao fao ideia do por que.

--- WebCommerce/Servicos/ServicoEmail.cs
+++ WebCommerce/Servicos/ServicoEmail.cs
@@ -18,26 +18,16 @@ namespace WebCommerce.Servicos
     {
         public static Task EnviarEmailAsync(string email, string assunto, string mensagem)
         {
-            //serviço paraenviar email
+            //serviço para enviar email
             var minhaMensagem = new SendGridMessage();
             minhaMensagem.AddTo(email);
-<<<<<<< HEAD
             minhaMensagem.From = new MailAddress("webcommerce.unit@outlook.com", "WebCommerce Unit");
             minhaMensagem.Subject = assunto;
             minhaMensagem.Text = mensagem;
             minhaMensagem.Html = mensagem;
-            var credenciais = new NetworkCredential("Usuario aqui", "Senha aqui");
+            var credenciais = new NetworkCredential("usuario aqui", "senha aqui");
+            
             //Cria um transporte web para enviar email
-=======
-            minhaMensagem.From = new MailAddress("deivizika@hotmail.com", "Deivide Mateus");
-            minhaMensagem.Subject = assunto;
-            minhaMensagem.Text = mensagem;
-            minhaMensagem.Html = mensagem;
-            var credenciais = new NetworkCredential("deivide_mateus","dg88013588");
-            //Cria um transporte web para enviar email
-            var transportWeb = new Web(credenciais);
-            // Cria um transporte web para enviar email
->>>>>>> parent of c3f58df... Merge branch 'master' of https://github.com/andrext660/WebCommerce
             var transporteWeb = new Web(credenciais);
             // Envia o email
             if (transporteWeb != null)
