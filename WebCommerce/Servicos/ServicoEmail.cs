39e0d4c6a5a9e5f52ed55b92cd2f1ec4e9ce4f7e
Deivide Mateus <deivide.mateus@hotmail.com>
2018-11-21 23:24:23 -0300

    Revertendo

--- WebCommerce/Servicos/ServicoEmail.cs
+++ WebCommerce/Servicos/ServicoEmail.cs
@@ -16,17 +16,28 @@ namespace WebCommerce.Servicos
 {
     public class ServicoEmail
     {
-        public static Task EnviaEmailAsync(string email, string assunto, string mensagem)
+        public static Task EnviarEmailAsync(string email, string assunto, string mensagem)
         {
-            // serviço para enviar email
+            //serviço paraenviar email
             var minhaMensagem = new SendGridMessage();
             minhaMensagem.AddTo(email);
+<<<<<<< HEAD
             minhaMensagem.From = new MailAddress("webcommerce.unit@outlook.com", "WebCommerce Unit");
             minhaMensagem.Subject = assunto;
             minhaMensagem.Text = mensagem;
             minhaMensagem.Html = mensagem;
             var credenciais = new NetworkCredential("Usuario aqui", "Senha aqui");
             //Cria um transporte web para enviar email
+=======
+            minhaMensagem.From = new MailAddress("deivizika@hotmail.com", "Deivide Mateus");
+            minhaMensagem.Subject = assunto;
+            minhaMensagem.Text = mensagem;
+            minhaMensagem.Html = mensagem;
+            var credenciais = new NetworkCredential("deivide_mateus","dg88013588");
+            //Cria um transporte web para enviar email
+            var transportWeb = new Web(credenciais);
+            // Cria um transporte web para enviar email
+>>>>>>> parent of c3f58df... Merge branch 'master' of https://github.com/andrext660/WebCommerce
             var transporteWeb = new Web(credenciais);
             // Envia o email
             if (transporteWeb != null)
